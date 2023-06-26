dcde75ba5f5ea1e2e56c59b3cd69ed995f5774a9
Simone Cuomo <simone@thisdot.co>
2023-06-26 12:47:02 +0100

    Initialise Vue application

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # 100-ms-vue
-Showcase of 100ms using Vue as the framework of choice
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Run your unit tests
+```
+yarn test:unit
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <h1>Tensorflow video demo</h1>>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+</style>
